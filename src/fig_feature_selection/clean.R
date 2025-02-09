8471ea28e245e8da9a46c95e0c754c5af5855ed5
Tony Liang <chunqingliang@gmail.com>
2025-02-08 20:36:50 -0800

    fix: remove redundant rename

--- src/fig_feature_selection/clean.R
+++ src/fig_feature_selection/clean.R
@@ -23,6 +23,7 @@ source(here::here("src/common_helpers.R"))
 # Custom function
 wrangle_feat_selection <- function(df) {
   df %>%
+  rename(dataset = dataset_name) %>%
   # Need to fix error for mogonet appending view in front of feature
   mutate(
     feature = case_when(
@@ -38,8 +39,8 @@ wrangle_feat_selection <- function(df) {
     # SGCCA has slight problem in missing a feature in tcga-brca and tcga-kipan???
     # So need to drop this
     filter(! (
-      (feature == "RNAseq_HiSeq_Gene_level_GAGE1" & dataset_name == "tcga-brca") |
-        (feature == "RNAseq_HiSeq_Gene_level_C8orf71" & dataset_name == "tcga-kipan")
+      (feature == "RNAseq_HiSeq_Gene_level_GAGE1" & dataset == "tcga-brca") |
+        (feature == "RNAseq_HiSeq_Gene_level_C8orf71" & dataset == "tcga-kipan")
     )
     ) %>%
     # Rename the method names
@@ -57,7 +58,7 @@ clean_real <- function(df, cor_method="spearman") {
   # Some intermediate dataframes here for using later
   # Get rankings first
   ranking_df <- df %>%
-    group_by(method, dataset_name, view) %>%
+    group_by(method, dataset, view) %>%
     # So the coef with rank number smaller means better
     # ie. rank 1 (highest) > rank2 > ... rank 10 > ... rank n
     mutate(ranking = rank(desc(abs(coef)))) %>%
@@ -66,14 +67,13 @@ clean_real <- function(df, cor_method="spearman") {
   # And pivot it to get correlation matri
   cor_mat <- ranking_df %>%
     pivot_wider(names_from = method, values_from=ranking) %>%
-    rename(dataset = dataset_name) %>%
     filter(dataset_type == "real") %>%
     select_if(is.numeric) %>%
     select(order(colnames(.))) %>%
     as.matrix() %>%
     cor(method = cor_method)
   # And specifically let the colnames to be dataset
-  colnames(cor_mat) <- ranking_df$dataset_name |> unique() |> sort()
+  colnames(cor_mat) <- ranking_df$dataset |> unique() |> sort()
 
   return(cor_mat)
 
@@ -88,14 +88,12 @@ clean_sim <- function(feat_result_df) {
 
   # Separate to get all counts
     all_counts_df <- feat_result_df %>%
-      rename(dataset = dataset_name) %>%
       group_by(method, dataset, view, feature_type) %>%
       summarize(n = n()) %>%
       ungroup()
 
 
     merged_df <- feat_result_df %>%
-      rename(dataset = dataset_name) %>%
       group_by(method, dataset, view) %>%
         arrange(desc(abs(coef)), .by_group = TRUE) %>%
       group_map(~ {
@@ -125,7 +123,7 @@ clean_sim <- function(feat_result_df) {
   top_identifier = "top_counts"
   bottom_identifier = "bottom_counts"
 
-  a_df <- merged_df %>%
+  flat_bin_metric_df <- merged_df %>%
     group_by(method, dataset, view) %>%
     summarize(
       TP = sum(n_selected[category == top_identifier & feature_type == "real"], na.rm = TRUE),
@@ -136,7 +134,7 @@ clean_sim <- function(feat_result_df) {
       .groups = "drop"
     )
 
-  a_df %>%
+  flat_bin_metric_df %>%
     group_by(method, dataset, view) %>%
     summarize(sensitivity = TP / (TP + FN),
 
@@ -151,7 +149,7 @@ clean_sim <- function(feat_result_df) {
     retrieve_sim_params()
 }
 
-#input_path <- "data/raw/real_data_results/all_feature_selection_results.csv"
+input_path <- "data/raw/real_data_results/all_feature_selection_results.csv"
 #input_path <- "data/raw/simulated_data_results/all_feature_selection_results.csv"
 
 main <- function(input_path, output_path, data_type=c("real", "sim")) {
@@ -161,7 +159,6 @@ main <- function(input_path, output_path, data_type=c("real", "sim")) {
   feat_result_df <- data.table::fread(input_path) %>%
     as_tibble() %>%
     wrangle_feat_selection()
-
 
 
   # # Handle data type-specific processing
