1487e1f93a37c080bd9d75494ee07643172f3215
Tony Liang <chunqingliang@gmail.com>
2025-07-15 18:56:07 -0700

    feat: update docs, refactor code for plotting fgsea

---
+++ src/fig_fgsea_analysis/03a_plot_fgsea_part1.R
@@ -0,0 +1,162 @@
+doc <- "
+
+This script is used to plot fgsea part 1 results
+
+Usage:
+  03a_plot_fgsea_part1.R [options]
+
+Options:
+  --input_path=INPUT_PATH       File to load fgsea part1 summary
+  --output_path=OUTPUT_PATH     File to output the plot data
+  --width=WIDTH                 Width of the figure [default: 10]
+  --height=HEIGHT               Height of the figure [default: 8]
+"
+
+
+library(ggplot2)
+library(tidytext)
+library(data.table)
+suppressPackageStartupMessages(library(dplyr))
+
+get_method_custom_colors <- function(method_palette="Paired") {
+  # This fun is to match the color choices used for the methods
+  custom_method_palette <-  RColorBrewer::brewer.pal(n = 12, name = method_palette)
+  method_order_names <- c(
+    "diablo-full_ncomp-1",
+    "diablo-full_ncomp-2",
+    "diablo-null_ncomp-1",
+    "diablo-null_ncomp-2",
+    "mofa-Factor1 + glmnet",
+    "mofa-Factor2 + glmnet",
+    "mogonet",
+    "multiview",
+    "rgcca-full_ncomp-1 + lda",
+    "rgcca-null_ncomp-1 + lda",
+    "rgcca-full_ncomp-2 + lda",
+    "rgcca-null_ncomp-2 + lda"
+  )
+  names(custom_method_palette) <- method_order_names
+  return(custom_method_palette)
+}
+
+plot_bar <- function(data, custom_method_palette) {
+  significant_pathways_method_gs_plot_obj <- data %>%
+    # And reorder it for plotting
+    dplyr::mutate(label_reordered = reorder_within(method, mean_n_sig, gs_collection_name)) %>%
+    ggplot(aes(x = label_reordered, y = mean_n_sig, fill = method)) +
+    geom_errorbar(aes(ymin = mean_n_sig, ymax = mean_n_sig + sd_n_sig),
+                  width = 0.2, position = position_dodge(0.9)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~ gs_collection_name, scales = "free") +
+    labs(x = "Method", y = "Mean # Significant Pathways",
+         title = "Significant Pathways by Method and Gene Set Collection") +
+    theme_bw() +
+    tidytext::scale_x_reordered() +
+    scale_y_log10(expand = expansion(mult = c(0, 0.1))) +
+    coord_flip() +
+    scale_fill_manual(values = custom_method_palette) +
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      panel.grid.major.y = element_blank()
+    )
+
+  return(significant_pathways_method_gs_plot_obj)
+}
+
+main <- function(input_path, output_path, width, height, method_palette="Paired") {
+  if (is.null(input_path)) {
+    input_path <- "data/processed/fgsea_part1_summary_df.csv"
+  }
+
+  if (is.null(output_path)) {
+    output_path <- "data/processed/fig_fgsea_panel_a_plot_data.rds"
+  }
+
+  combined_summary <- fread(here::here(input_path))
+  # Get the color palette for methods
+  custom_method_palette <- get_method_custom_colors(method_palette)
+  # Get the plot object
+  significant_pathways_method_gs_plot_obj <- plot_bar(combined_summary, custom_method_palette)
+
+  # Save the figure
+  ggsave(
+    filename = "results/figures/fig_fgsea_panel_a.png",
+    plot = significant_pathways_method_gs_plot_obj,
+    width = width,
+    height = height
+  )
+
+  # And save the plot data as rds
+  saveRDS(significant_pathways_method_gs_plot_obj,
+          file = here::here(output_path))
+  message("\nSaved plot data for fgsea part 1 into ", output_path)
+
+}
+
+palette <- "Paired"
+
+opt <- docopt::docopt(doc)
+main(input_path=opt$input_path, output_path=opt$output_path,
+     width=as.numeric(opt$width),
+     height=as.numeric(opt$height),
+     method_palette=palette)
+
+
+# =====================================
+# Then in a summarized way, on average for each method
+
+# ==============================================
+# THIS IS C2
+
+#
+# c2_summarized_n_sig_plot <- c2_summarized_method %>%
+#   ggplot(
+#     aes(x = reorder(method, -mean_n_sig),
+#         y = mean_n_sig,
+#         fill = method)
+#   ) +
+#   geom_errorbar(aes(
+#     ymin = mean_n_sig, ymax = mean_n_sig + sd_n_sig, width = 0.6)
+#   ) +
+#   geom_bar(stat = "identity") +
+#   labs(x = "Method", y = "Mean Number of Pathways per Dataset",
+#        title = "Mean Number of Pathways Detected per Method") +
+#   theme_bw() +
+#   scale_y_log10() +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+#   scale_fill_brewer(palette = palette)
+#
+# c2_summarized_n_sig_plot
+#
+#
+#
+# ggsave("c2_summarized_n_sig.png",
+#        plot = c2_summarized_n_sig_plot,
+#        width=width, height=height)
+#
+# # =================================================
+# # THIS IS C6
+#
+# c6_summarized_n_sig_plot <- c6_summarized_method %>%
+#   ggplot(
+#     aes(x = reorder(method, -mean_n_sig),
+#         y = mean_n_sig,
+#         fill = method)
+#   ) +
+#
+#   geom_errorbar(aes(ymin = mean_n_sig, ymax = mean_n_sig + sd_n_sig, width=0.6)) +
+#   geom_bar(stat = "identity") +
+#
+#
+#   labs(x = "Method", y = "Mean Number of Pathways per Dataset",
+#        title = "Mean Number of Pathways Detected per Method") +
+#   theme_bw() +
+#   scale_y_log10() +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+#   scale_fill_brewer(palette = palette)
+#
+# c6_summarized_n_sig_plot
+#
+# ggsave("c6_summarized_n_sig.png",
+#        plot = c6_summarized_n_sig_plot,
+#        width=width, height=height)
---
+++ src/fig_fgsea_analysis/03b_plot_fgsea_part2.R
@@ -0,0 +1,236 @@
+doc <- "
+
+This script is used to plot fgsea part 2 results
+
+Usage:
+  03b_plot_fgsea_part2.R [options]
+
+Options:
+  --input_path=INPUT_PATH       File to load fgsea part1 summary
+  --output_path=OUTPUT_PATH     File to output the plot data
+  --width=WIDTH                 Width of the figure [default: 12]
+  --height=HEIGHT               Height of the figure [default: 9]
+"
+
+
+library(data.table)
+library(ComplexHeatmap)
+library(ggplot2)
+library(circlize)
+library(cowplot)
+suppressPackageStartupMessages(library(dplyr))
+
+# Load other utilities
+source(here::here("src/fig_fgsea_analysis/_utils.R"))
+
+# Custom function for plotting
+
+get_text_color <- function(fill_color) {
+  rgb <- col2rgb(fill_color)
+  luminance <- (0.299 * rgb[1] + 0.587 * rgb[2] + 0.114 * rgb[3]) / 255
+  ifelse(luminance < 0.5, "white", "black")
+}
+
+get_method_custom_colors <- function(method_palette="Paired") {
+  # This fun is to match the color choices used for the methods
+  custom_method_palette <-  RColorBrewer::brewer.pal(n = 12, name = method_palette)
+  method_order_names <- c(
+    "diablo-full_ncomp-1",
+    "diablo-full_ncomp-2",
+    "diablo-null_ncomp-1",
+    "diablo-null_ncomp-2",
+    "mofa-Factor1 + glmnet",
+    "mofa-Factor2 + glmnet",
+    "mogonet",
+    "multiview",
+    "rgcca-full_ncomp-1 + lda",
+    "rgcca-null_ncomp-1 + lda",
+    "rgcca-full_ncomp-2 + lda",
+    "rgcca-null_ncomp-2 + lda"
+  )
+  names(custom_method_palette) <- method_order_names
+  return(custom_method_palette)
+}
+
+# =============================================================================
+# PLOTTING OF THE HEATMAP
+# =============================================================================
+plot_heatmap <- function(plot_matrix, annotation_table, custom_method_palette) {
+  # =========================================================
+  # Then the annotations here
+  row_ha <- rowAnnotation(
+    Method = factor(
+      annotation_table$method,
+      levels = unique(annotation_table$method)
+    ),
+    col = list(Method = custom_method_palette),
+    annotation_legend_param = list(
+      Method = list(
+        #title_gp = gpar(fontsize = 16),
+        #labels_gp = gpar(fontsize = 8),
+        nrow=4
+      )
+    ),
+    show_annotation_name = FALSE
+    #Dataset = factor(annotation_table$dataset, levels = unique(annotation_table$dataset))
+  )
+
+  col_ha <- columnAnnotation(
+    # First 4 columns are "method", "dataset", "n", "group_num", so that's why
+    # start from 5
+    Organ = factor(annotation_table[5:ncol(annotation_table)] %>%
+                     colnames()),
+    show_annotation_name = FALSE
+  )
+
+  # Add number into the heatmap to show the actual ratio
+  # Function of color
+  col_fun <- colorRamp2(c(0, 0.5, 1), c("white", "steelblue1", "blue"))
+  #col_fun <- colorRamp2(c(0, 0.5, 1), c("white", "gainsboro", "lightgrey"))
+  # Leave this as is, if not then go back to patchy problem
+  # Y-axis to actual name of the dataset
+  # Add own labelling vector to "link" dataset, since
+  # they could be colated
+  # Row split could be customized
+  htmp <- Heatmap(
+    plot_matrix,
+    name = "Proportion of significant cells",
+    col = col_fun,
+    right_annotation = row_ha,
+    # This map_name is custom function found in _utils.R of the same dir
+    row_split = annotation_table$dataset %>% map_name(),
+    cluster_rows = TRUE,
+    #column_split = annotation_table$method,
+    border = TRUE,
+    row_title_rot = 0,
+    row_gap = unit(2, "mm"),
+    column_names_rot = 45,
+    row_names_gp = gpar(fontsize = 10),
+    row_title_gp = gpar(fontsize = 10),
+    show_row_names = FALSE,
+    cluster_columns = TRUE,
+    #cluster_rows = FALSE,
+    row_dend_width = unit(1.5, "cm"),
+    cell_fun = function(j, i, x, y, width, height, fill) {
+      text_color <- get_text_color(fill)
+      grid.text(sprintf("%.2f", plot_matrix[i, j]), x, y,
+                gp = gpar(col = text_color, fontsize = 10))
+    },
+    # Assign legend
+    heatmap_legend_param = list(
+      at = c(0, 0.5, 1),
+      labels = c("Low", "", "High"),
+      grid_height = unit(0.3, "cm"), grid_width = unit(0.3, "cm"),
+      legend_width = unit(5, "cm"),
+      #labels_gp = gpar(fontsize = text_size - 5),
+      legend_direction = "horizontal"
+      #legend_direction = "vertical"
+    ),
+
+  )
+
+  # Draw heatmap and convert to grob
+  ht_grob <- grid.grabExpr(
+    #draw(ht, heatmap_legend_side="bottom", annotation_legend_side="right",
+    #     legend_grouping = "original")
+    draw(htmp, merge_legends = TRUE,
+         heatmap_legend_side = "bottom",
+         annotation_legend_side = "bottom")
+  )
+  return(ht_grob)
+}
+# =============================================================================
+# PLOTTING OF THE BAR PLOT COUNT BASED ON HEATMAP
+# =============================================================================
+plot_annot_bar <- function(methods, custom_method_palette) {
+  # The frequency plot
+  annot_bar_plot <- methods %>%
+    table() %>%
+    tibble::enframe(name="method", value="n") %>%
+    mutate(n = as.integer(n)) %>%
+    ggplot(aes(x=reorder(method, n), y=n, fill=method)) +
+    geom_bar(stat="identity", width=0.7) +
+    labs(x="Method", y="Frequency", fill="Method")+
+    scale_fill_manual(values = custom_method_palette) +
+    theme_bw() +
+    # And remove horizontal lines
+    theme(
+      panel.grid.major.y = element_blank()
+    ) +
+    coord_flip()
+  return(annot_bar_plot)
+}
+
+main <- function(input_path, output_path, width, height, method_palette="Paired") {
+  if (is.null(input_path)) {
+    input_path <- "data/processed/fgsea_part2_summary_df.csv"
+  }
+
+  if (is.null(output_path)) {
+    output_path <- "data/processed/fig_fgsea_panel_b_plot_data.rds"
+  }
+
+  # Load the data here
+  annotation_table <- fread(here::here(input_path))
+  # Extract the plot matrix from annotations
+  plot_matrix <- annotation_table %>%
+    dplyr::select(-c("method", "dataset", "n", "group_num")) %>%
+    as.matrix()
+  # Also get the custom method colors
+  custom_method_palette <- get_method_custom_colors(method_palette=method_palette)
+
+  # The heatmap plot goes here
+  ht_grob <- plot_heatmap(
+    plot_matrix=plot_matrix, annotation_table=annotation_table,
+    custom_method_palette=custom_method_palette
+  )
+
+  # Count method appearance as bars
+  annot_bar_plot <- plot_annot_bar(
+    methods=annotation_table$method,
+    custom_method_palette=custom_method_palette
+  )
+
+  # And combine them together
+  output_plot <- plot_grid(
+    ggdraw(ht_grob),
+    annot_bar_plot +
+      xlab(NULL) +
+      guides(fill="none"),
+    ncol = 2,
+    rel_widths = c(2, 1)
+  )
+
+  ggsave("results/figures/fig_fgsea_panelB.png", plot=output_plot, width=width, height=height)
+
+
+  saveRDS(output_plot, here::here(output_path))
+  message("\nSaved plot data for fgsea part 2 into ", output_path)
+
+}
+
+palette <- "Paired"
+
+opt <- docopt::docopt(doc)
+main(input_path=opt$input_path, output_path=opt$output_path,
+     width=as.numeric(opt$width),
+     height=as.numeric(opt$height),
+     method_palette=palette)
+
+
+
+# THIS IS STALE CODE
+#
+# methods <- annotation_table$method |> unique() |> sort()
+# #
+# # # Get a pastel color palette with the same number of colors as your methods
+# n_methods <- length(methods)
+# # # RColorBrewer's Pastel1 has up to 9 colors; use Pastel2 if you need more variation
+# pastel_colors <- RColorBrewer::brewer.pal(n = n_methods, name = "Paired")
+# #
+# # # Create a named vector of colors for each method
+# method_colors <- setNames(pastel_colors, methods)
+
+
+
+
