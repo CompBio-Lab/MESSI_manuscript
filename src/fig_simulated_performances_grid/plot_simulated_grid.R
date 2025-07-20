a3febb264f3a8127bd23e1e34149c3b7c09740b8
Tony Liang <chunqingliang@gmail.com>
2025-07-19 21:08:25 -0700

    knit: 2025-07-19 use barplot for the grid

--- src/fig_simulated_performances_grid/plot_simulated_grid.R
+++ src/fig_simulated_performances_grid/plot_simulated_grid.R
@@ -74,7 +74,31 @@ main <- function(input_path, output_path, width, height) {
       )
 
   # Then plot it as point and lines
-  output_plot <- plot_line_point_grid(plot_data)
+  #output_plot <- plot_line_point_grid(plot_data)
+
+
+  output_plot <- plot_data |>
+    ggplot(
+      aes(
+        x = method, y = value, fill = corr,  group=corr)
+    ) +
+    geom_bar(position = "dodge2", stat="identity") +
+    #geom_line(position = position_dodge(width = 0.4)) +
+    facet_grid(metric~signal) +
+    labs(y = "Performance Metric Value", x = "Method", color = "Correlation") +
+    theme_bw(base_size = 14) +
+    custom_theme_for_sim_plot() +
+    scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 12)) +
+    #scale_color_manual(
+    #  values = c("0" = "#D55E00", "0.5" = "#0072B2", "1" = "#009E73"),
+    #  labels = c("0" = "Low", "0.5" = "Medium", "1" = "High")
+    #) +
+    theme(legend.position = "bottom") +
+    guides(color = guide_legend(
+      title.position = "top",
+      label.position = "bottom",
+      direction = "horizontal"
+    ))
 
   # Save the figure
   ggsave(
