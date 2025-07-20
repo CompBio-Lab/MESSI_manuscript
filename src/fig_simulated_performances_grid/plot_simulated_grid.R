2b87c685a23b48a44730dc2bb96c5c8888e272e4
Tony Liang <chunqingliang@gmail.com>
2025-07-20 16:34:53 -0700

    fix: finalize to barplot and use blue color

--- src/fig_simulated_performances_grid/plot_simulated_grid.R
+++ src/fig_simulated_performances_grid/plot_simulated_grid.R
@@ -20,6 +20,7 @@ Options:
 opt <- docopt::docopt(doc)
 
 # Load libraries
+suppressPackageStartupMessages(library(dplyr))
 library(ggplot2)
 
 # Load plotting helpers like colors
@@ -40,7 +41,8 @@ plot_line_point_grid <- function(plot_data) {
     custom_theme_for_sim_plot() +
     scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 12)) +
     scale_color_manual(
-      values = c("0" = "#D55E00", "0.5" = "#0072B2", "1" = "#009E73"),
+      #values = c("0" = "#D55E00", "0.5" = "#0072B2", "1" = "#009E73"),
+      values = c("0" = "#F4A6A6", "0.5" = "#A6DCEF", "1" = "#B9FBC0"),
       labels = c("0" = "Low", "0.5" = "Medium", "1" = "High")
     ) +
     theme(legend.position = "bottom") +
@@ -65,36 +67,50 @@ main <- function(input_path, output_path, width, height) {
   }
 
   # First load plot data and apply factoring
-  plot_data <- data.table::fread(input_path) |>
-    dplyr::mutate(
-      metric = factor(metric, levels = c("auc", "sensitivity", "specificity"),
-                    labels = c("AUC", "Sensitivity", "Specificity")),
-      signal = factor(signal, labels = c("Signal: Low    ", "Signal: Medium ", "Signal: High   ")),
-      corr = as.factor(corr)
-      )
+  plot_data <- data.table::fread(input_path)
+
 
   # Then plot it as point and lines
   #output_plot <- plot_line_point_grid(plot_data)
 
+  # Correct order should be: mofa-F1, mogonet- multiview?
+  subset_for_order <- plot_data |>
+    filter(signal == "3", metric == "sensitivity") |>
+    group_by(method) |>
+    summarize(mean_value = mean(value, na.rm = TRUE)) |>
+    arrange(mean_value) |>
+    dplyr::pull(method)
+
 
   output_plot <- plot_data |>
+    dplyr::mutate(
+      metric = as.factor(metric),
+      signal = factor(signal, labels = c("Signal: Low", "Signal: Medium ", "Signal: High")),
+      corr = as.factor(corr),
+      method = factor(method, levels=subset_for_order)
+    ) |>
     ggplot(
       aes(
         x = method, y = value, fill = corr,  group=corr)
     ) +
-    geom_bar(position = "dodge2", stat="identity") +
+    geom_bar(stat="identity",  position = position_dodge2(padding=0.4)) +
     #geom_line(position = position_dodge(width = 0.4)) +
     facet_grid(metric~signal) +
-    labs(y = "Performance Metric Value", x = "Method", color = "Correlation") +
+    labs(y = "Performance Metric Value", x = "Method", fill = "Correlation") +
     theme_bw(base_size = 14) +
     custom_theme_for_sim_plot() +
     scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 12)) +
-    #scale_color_manual(
-    #  values = c("0" = "#D55E00", "0.5" = "#0072B2", "1" = "#009E73"),
-    #  labels = c("0" = "Low", "0.5" = "Medium", "1" = "High")
-    #) +
+    scale_fill_manual(
+      #values = c("0" = "#D55E00", "0.5" = "#0072B2", "1" = "#009E73"),
+      #values = c("0" = "#F4A6A6", "0.5" = "#A6DCEF", "1" = "#B9FBC0"),
+      #values = c("0" = "#E69F00", "0.5" = "#56B4E9", "1" = "#009E73"),
+      #values = c("0" = "#E0BBE4", "0.5" = "#C084D3", "1" = "#6A51A3"),
+      values = c("0" = "#C6DBEF", "0.5" = "#6BAED6", "1" = "#2171B5"),
+
+      labels = c("0" = "Low", "0.5" = "Medium", "1" = "High")
+    ) +
     theme(legend.position = "bottom") +
-    guides(color = guide_legend(
+    guides(fill = guide_legend(
       title.position = "top",
       label.position = "bottom",
       direction = "horizontal"
