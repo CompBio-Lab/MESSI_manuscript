fb82c4430281b765e66c7dd3aada0f56200d438c
Tony Liang <chunqingliang@gmail.com>
2026-02-27 19:08:06 -0800

    fix: update common legend for multimodal

--- plot_scripts/fig_multimodal_composite.R
+++ plot_scripts/fig_multimodal_composite.R
@@ -10,6 +10,7 @@ for (script in src_scripts) {
 library(cowplot)
 
 auc_perf <- readRDS("results/multimodal/fig5b_multimodal_auc_performance_dot.rds")
+prop_bar <- readRDS("results/multimodal/fig5f_multimodal_dataset_positive_prop_bar.rds")
 sig_pathways_count <- readRDS("results/multimodal/fig5c_multimodal_sig_pathways_count.rds")
 runtime <- readRDS("results/multimodal/fig5g_multimodal_runtime_vs_dataset_size.rds")
 memory <- readRDS("results/multimodal/fig5h_multimodal_memory_vs_dataset_size.rds")
@@ -18,27 +19,58 @@ top_pathway_ht <- readRDS("results/multimodal/fig5e_multimodal_top_pathways_iden
 
 
 
+bottom_compute <- plot_grid(
+  runtime + theme(legend.position = "none"),
+  memory +
+    theme(legend.position = "none"),
+  nrow=2,
+  labels=c("E", "F")
+)
+
+method_legend <- ggdraw(
+  get_legend_35(
+    sig_pathways_count +
+      guides(
+        fill=guide_legend(
+          nrow=2,
+          title=NULL
+        ),
+        shape = "none",
+        color = "none"
+      )
+  )
+)
+
 # So arranging these as 4 columns?
 bottom_right <- plot_grid(
   sig_pathways_count +
     theme(legend.position = "none") +
     ggtitle(NULL),
-  runtime + theme(legend.position = "top"),
-  memory +
-    theme(legend.position = "none"),
-  nrow=3,
-  labels = c("C", "D", "E")
+  # Shared legend
+  method_legend,
+  # Continue plot
+  bottom_compute,
+  ncol=1,
+  rel_heights = c(0.6, 0.08, 0.9),
+  labels = c("D", NULL, NULL)
   )
 
 
+bottom_left_top <- plot_grid(
+  auc_perf +
+    theme(legend.position = "bottom"),
+  prop_bar,
+  nrow=2,
+  rel_heights = c(0.9, 0.25),
+  labels=c("B", "C")
+)
 
 
 bottom_left <- plot_grid(
-  auc_perf +
-    theme(legend.position = "bottom"),
+  bottom_left_top,
   bottom_right,
-  ncol=2,
-  labels = c("B", NULL)
+  ncol=2
+  #labels = c(NU, NULL)
   )
 
 
@@ -46,7 +78,7 @@ bottom_middle <- plot_grid(
   jaccard_ht, top_pathway_ht,
   nrow = 2,
   rel_heights = c(0.55, 0.85),
-  labels = c("F", "G")
+  labels = c("G", "H")
 )
 
 
@@ -59,4 +91,7 @@ bottom <- plot_grid(
   rel_widths = c(0.4,0.6)
 )
 
-ggsave("fig5_multimodal_data_performances.png", bottom, width = 25, height = 15)
+ggsave("fig5_multimodal_data_performances.png",
+       bottom, width = 25, height = 15,
+       bg="white")
+
