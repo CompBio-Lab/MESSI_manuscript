88546e2665ed3c3b24884d9d6e71b518c0fdce16
Tony Liang <chunqingliang@gmail.com>
2026-03-11 02:07:21 -0700

    fix: move all the utils to common_helpers

---
+++ src/common_helpers/performance_evaluation_utils.R
@@ -0,0 +1,56 @@
+suppressPackageStartupMessages(library(dplyr))
+library(tibble)
+library(here)
+library(stringr)
+library(tidyr)
+
+source("src/common_helpers/standardize_data_funs.R")
+
+wrangle_bulk_data <- function(df) {
+  wrangle_df <- df %>%
+    dplyr::rename(method = method_name) %>%
+    # Given there's same result for rgcca and sgcca
+    # going to drop those of rgcca and retain sgcca only.
+    #filter(!str_detect(method, "sgcca")) %>%
+    # Remove the first component results from diablo
+    #filter(!str_detect(method, "diablo.*ncomp-1")) %>%
+    group_by(method, dataset) %>%
+    #summarise(
+    #  across(
+    #    .cols=c(auc, f1_score),
+    #    .fns=list(mean = mean, sd = sd)),
+    #  .groups = "drop"
+    #) %>%
+    #ungroup() %>%
+    # Then rank the performance of each method within each dataset
+    # I.e. For Dataset L, could be m1, m3 , m5, m2 , m6 from best to worst (left to right)
+    group_by(dataset) %>%
+    # TODO:  ~~Sort in desceding order, and rank them, i.e. 1st equals top performing~~
+    #mutate(ranking = rank(desc(auc_mean))) %>%
+    mutate(ranking = rank(auc)) %>%
+    ungroup() %>%
+    # # Rename method names
+    # mutate(
+    #   method = case_when(
+    #     str_detect(method, "mofa") ~ "mofa + glmnet",
+    #     str_detect(method, "rgcca") ~ paste0(method, " + lda"),
+    #     str_detect(method, "cooperative") ~ "multiview",
+    #     TRUE ~ method
+    #   )
+    #) %>%
+    # UGGLY FIX, append suffix of Factor to MOFA, for bulk data we set factor to 2
+    mutate(
+      method = case_when(
+        str_detect(tolower(method),"mofa") ~ str_c(method, "-Factor2"),
+      TRUE ~ method
+      )
+    ) %>%
+    # Capitalize or to upper the method names
+    mutate(method = standardize_method_names(method, "perf")) %>%
+    dplyr::select(
+      method, dataset, ranking,
+      auc, f1_score
+    ) %>%
+    arrange(ranking)
+  return(wrangle_df)
+}
